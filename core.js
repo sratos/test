5dd68ebd8952c7843ae39228ac3e6bf2e1112399
aki4 <aki4dev@gmail.com>
2016-05-04 15:13:10 +0300

    Code optimization

--- core.js
+++ core.js
@@ -8,21 +8,11 @@ function shuffle(a) {
     }
 }
 
-window.Test = {
-    list: [
-              ['_ЗАОЧ_КРОК1 Пробное тестирование - 2016', null],
-              ['_ЗАОЧ_КРОК1_Физ. химия (обучение)', 'fiz_himia'],
-              ['_ЗАОЧ_КРОК1_Фармакология (обучение)', 'farmakologia'],
-              ['_ЗАОЧ_КРОК1_Пат. физиология (обучение)', 'pat_fiziologia'],
-              ['_ЗАОЧ_КРОК1_Органич. химия (обучение)', 'organich_himia'],
-              ['_ЗАОЧ_КРОК1_Микробиология (обучение)', 'mikrobiologia'],
-              ['_ЗАОЧ_КРОК1_Ботаника (обучение)', 'botanika'],
-              ['_ЗАОЧ_КРОК1_Биохимия (обучение)', 'biohimia'],
-              ['_ЗАОЧ_КРОК1_Аналитич. химия (обучение)', 'analit_himia']
-          ],
-    answered: [],
-    questions: [],
-    answer: function() {
+function Test() {
+    var th1s = this;
+    this.answered = [];
+    this.questions = [];
+    this.answer = function() {
         var isCorrect = true;
         var answers = $('.sbl-answer');
             answers.each(function(a) {
@@ -32,57 +22,57 @@ window.Test = {
                 return;
             }
         });
-        Test.update({isCorrectAnswer: isCorrect ? 1 : -1, answered: 1});
-        Test.endQuestion(isCorrect);
-    },
-    update: function(o) {
+        th1s.update({isCorrectAnswer: isCorrect ? 1 : -1, answered: 1});
+        th1s.endQuestion(isCorrect);
+    };
+    this.update = function(o) {
         if(o) {
-            if(!Test._base[Test._currentQuestion].state) {
-                Test._base[Test._currentQuestion].state = {};
+            if(!th1s._base[th1s._currentQuestion].state) {
+                th1s._base[th1s._currentQuestion].state = {};
             }
             for(var i in o) {
-                Test._base[Test._currentQuestion].state[i] = o[i];
+                th1s._base[th1s._currentQuestion].state[i] = o[i];
             }
         }
         var tbl = {wrong: 0, correct: 0, forgot: 0, na: 0, a: 0};
-        for(var i in Test.answered) {
-            if (Test.answered[i].state.isCorrectAnswer != null)tbl.a++;
+        for(var i in th1s.answered) {
+            if (th1s.answered[i].state.isCorrectAnswer != null)tbl.a++;
 
-            if (Test.answered[i].state && Test.answered[i].state.forgot == 1) tbl.forgot++;
-            if (Test.answered[i].state.isCorrectAnswer == 1)  tbl.correct++;
-            else if (Test.answered[i].state.isCorrectAnswer == -1) tbl.wrong++;
+            if (th1s.answered[i].state && th1s.answered[i].state.forgot == 1) tbl.forgot++;
+            if (th1s.answered[i].state.isCorrectAnswer == 1)  tbl.correct++;
+            else if (th1s.answered[i].state.isCorrectAnswer == -1) tbl.wrong++;
         }
-        for(var i in Test._base) {
-            if (!Test._base[i].state) continue;
-            if (Test._base[i].state.isCorrectAnswer != null)tbl.a++;
+        for(var i in th1s._base) {
+            if (!th1s._base[i].state) continue;
+            if (th1s._base[i].state.isCorrectAnswer != null)tbl.a++;
 
-            if (Test._base[i].state && Test._base[i].state.forgot == 1) tbl.forgot++;
-            if (Test._base[i].state.isCorrectAnswer == 1)  tbl.correct++;
-            else if (Test._base[i].state.isCorrectAnswer == -1) tbl.wrong++;
+            if (th1s._base[i].state && th1s._base[i].state.forgot == 1) tbl.forgot++;
+            if (th1s._base[i].state.isCorrectAnswer == 1)  tbl.correct++;
+            else if (th1s._base[i].state.isCorrectAnswer == -1) tbl.wrong++;
         }
         $('#score-correct').text(tbl.correct);
         $('#score-wrong').text(tbl.wrong);
         $('#score-forgot').text(tbl.forgot);
-        $('#score-overall').text(Test.answered.length + '/' + (Test._base.length + Test.answered.length));
-    },
-    endTest: function() {
+        $('#score-overall').text(th1s.answered.length + '/' + (th1s._base.length + th1s.answered.length));
+    };
+    this.endTest = function() {
         $('#question-table').hide();
         $('#results-table').show();
         var questionsHtml = '';
         var correctQuestionsCount = 0;
-        for(var i in Test.answered) {
+        for(var i in th1s.answered) {
             var answersHtml = '';
-            for(var j in Test.answered[i].answers) {
-                answersHtml += '<div class="'+( Test.answered[i].answers[j].correct == 1 ? 'correct-answer' : '')+'">' + Test.answered[i].answers[j].name + '</div>';
+            for(var j in th1s.answered[i].answers) {
+                answersHtml += '<div class="'+( th1s.answered[i].answers[j].correct == 1 ? 'correct-answer' : '')+'">' + th1s.answered[i].answers[j].name + '</div>';
             }
-            questionsHtml += '<tr><td class="question-result ' + (Test.answered[i].state.isCorrectAnswer == 1 ? 'correct-result' : 'wrong-result')+'"><b class="result-question">'
-            questionsHtml += Test.answered[i].name +  '</b>'+answersHtml+'</td></tr>';
+            questionsHtml += '<tr><td class="question-result ' + (th1s.answered[i].state.isCorrectAnswer == 1 ? 'correct-result' : 'wrong-result')+'"><b class="result-question">'
+            questionsHtml += th1s.answered[i].name +  '</b>'+answersHtml+'</td></tr>';
 
-            if (Test.answered[i].state.isCorrectAnswer == 1)
+            if (th1s.answered[i].state.isCorrectAnswer == 1)
                 correctQuestionsCount++;
         }
 
-        var questionsCount = Test.answered.length;
+        var questionsCount = th1s.answered.length;
         resultPercent = Math.floor((correctQuestionsCount/questionsCount)*100);
 
         $('.score-percent').text(resultPercent + '%');
@@ -105,11 +95,11 @@ window.Test = {
         $('#results-score-correct').text($('#score-correct').text());
         $('#results-score-wrong').text($('#score-wrong').text());
         $('#results-score-forgot').text($('#score-forgot').text());
-    },
-    endQuestion: function (isRight) {
-            Test.answered.push(Test._base[Test._currentQuestion]);
-            Test._base.splice(Test._currentQuestion, 1);
-            Test.update(null);
+    };
+    this.endQuestion = function (isRight) {
+            th1s.answered.push(th1s._base[th1s._currentQuestion]);
+            th1s._base.splice(th1s._currentQuestion, 1);
+            th1s.update(null);
 
             $('#control-bar').hide();
             $('#next-bar').show();
@@ -131,82 +121,76 @@ window.Test = {
                 }
                 $(this).removeClass('selected-answer');
             });
-        },
-        move: function (o) {
+        };
+        this.move = function (o) {
             if (o != null) {
-                o.data.goNext ? Test._currentQuestion++ : Test._currentQuestion--;
+                o.data.goNext ? th1s._currentQuestion++ : th1s._currentQuestion--;
             }
 
-            if (Test._currentQuestion < 1)
-                Test._currentQuestion = Test._base.length - 1;
-            else if (Test._currentQuestion > Test._base.length - 1)
-                Test._currentQuestion = 0;
-            if (Test._base.length <= 0) {
-                Test.endTest();
+            if (th1s._currentQuestion < 1)
+                th1s._currentQuestion = th1s._base.length - 1;
+            else if (th1s._currentQuestion > th1s._base.length - 1)
+                th1s._currentQuestion = 0;
+            if (th1s._base.length <= 0) {
+                th1s.endTest();
                 return;
             }
-            var q = Test._base[Test._currentQuestion];
+            var q = th1s._base[th1s._currentQuestion];
             $('#block-question').text(q.name)
             $('#block-answers').empty();
             shuffle(q.answers);
             for (var j=0; j < q.answers.length; j++) {
                 $('#block-answers').append('<tr><td class="sbl-answer" correct="'+q.answers[j].correct+'"><span>'+q.answers[j].name+'</span></td></tr>')
             }
-        },
-    _currentQuestion: null,
-    _base: [],
-    Init: function () {
+        };
+    this._currentQuestion = null;
+    this._base = [];
 
-        Test.questions = [];
-        Test.answered = [];
-        Test._currentQuestion = 0;
-        Test._base = [];
-
-        $(document).on('click', '#question-table .sbl-answer', function() {
-            $(this).toggleClass('selected-answer');
-        })
-
-        var options = '';
-        for(var i in Test.list) {
-            options += '<option value="'+i+'">'+Test.list[i][0]+'</option>'
-        }
-        $('#test-list').append(options);
-
-        $('#sbtn-begin').on('click', function() {
+        this.startTest = function() {
             testId = $('#test-list').val();
             var data = [];
             if (testId == 0) {
-                for (var i=1; i < Test.questions.length; i++) {
-                    shuffle(Test.questions[i]);
-                    var q = Test.questions[i].splice(0,1); //20
+                for (var i=1; i < th1s.questions.length; i++) {
+                    shuffle(th1s.questions[i]);
+                    var q = th1s.questions[i].splice(0,1); //20
                     for(var j in q) {
                         data.push(q[j]);
                     }
                 }
             } else {
-                data = Test.questions[testId];
+                data = th1s.questions[testId];
             }
             shuffle(data);
-            Test._base = data;
-            Test._currentQuestion = 0;
-            Test.move(null);
+            th1s._base = data;
+            th1s._currentQuestion = 0;
+            th1s.move(null);
             $('#start-table').hide();
             $('#question-table').show();
-            Test.update(null);
+            th1s.update(null);
+        };
+
+    this.Init = function (q) {
+
+        th1s.questions = q;
+        th1s.answered = [];
+        th1s._currentQuestion = 0;
+        th1s._base = [];
+
+        $(document).on('click', '#question-table .sbl-answer', function() {
+            $(this).toggleClass('selected-answer');
         })
 
         $('.sbtn-exit').on('click', function() {
             if(window.confirm("Бросить тест и выйти?")) {
-                Test.Init();
                 $('#start-table').show();
                 $('#question-table').hide();
                 $('#results-table').hide();
             }
         });
 
-        $('#sbtn-next').on('click', {goNext: true}, Test.move);
-        $('#sbtn-prev').on('click', {goNext: false}, Test.move);
-        $('#sbtn-answer').on('click', Test.answer)
+        $('#sbtn-next').on('click', {goNext: true}, th1s.move);
+        $('#sbtn-prev').on('click', {goNext: false}, th1s.move);
+        $('#sbtn-answer').on('click', th1s.answer)
 
         $('#sbtn-iforgot').on('click', function () {
             $('.sbl-answer').each(function(a) {
@@ -215,7 +199,7 @@ window.Test = {
                     $(this).addClass('forgot-correct');
                 }
             });
-            Test.update({forgot:1});
+            th1s.update({forgot:1});
         });
 
         $('#next-bar').on('click', function() {
@@ -225,18 +209,8 @@ window.Test = {
             $('#rays').remove();
             $('#next-bar').hide();
             $('#control-bar').show();
-            Test.move(null);
+            th1s.move(null);
         });
 
-        for(var i in Test.list) {
-            if (Test.list[i][1] == null) continue;
-            fetchJson(Test.list[i][1] + ".json", i)
-        }
-
-        function fetchJson(name, iter) {
-            $.getJSON(name, function( data ) {
-                Test.questions[iter] = data;
-            });
-        }
-    }
+    };
 }
